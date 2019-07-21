6772c659c31c8fd9976a71b14616d1f5c70bfe06
ealkilicgil <ealkilicgil@gmail.com>
2019-07-21 21:00:30 +0300

    Updates

---
+++ precache-manifest.dc0e1a6f5fe6e83deefbfee5787c9fff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af6634298f208dfadc30a721d7df0a4",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4a516afcb47057f489ac",
+    "url": "/monster-rolodex/static/css/main.9ebee202.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4a516afcb47057f489ac",
+    "url": "/monster-rolodex/static/js/main.45890ae6.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
